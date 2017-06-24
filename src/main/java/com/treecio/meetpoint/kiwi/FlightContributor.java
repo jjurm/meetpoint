ee94e9de233ec90ea9f91dd04d95801ca95c85e0
padr31 <pali31pd@centrum.sk>
2017-06-24 14:56:11 +0200

    jetlag

--- src/main/java/com/treecio/meetpoint/kiwi/FlightContributor.java
+++ src/main/java/com/treecio/meetpoint/kiwi/FlightContributor.java
@@ -1,5 +1,6 @@
 package com.treecio.meetpoint.kiwi;
 
+import com.jjurm.projects.mpp.map.JetLagMap;
 import com.treecio.meetpoint.model.Contributor;
 import com.treecio.meetpoint.model.MeetingPossibility;
 import com.treecio.meetpoint.model.ContributorResult;
@@ -46,13 +47,18 @@ public class FlightContributor implements Contributor {
             double price = Double.parseDouble(actualObj.get("data").get(0).findValue("conversion").findValue("EUR").toString());
             double duration = Utils.timeToSeconds(actualObj.get("data").get(0).findValue("fly_duration").toString());
 
-            return new ContributorResult(price, 1, 1);
+            double lngFrom = Double.parseDouble(actualObj.get("data").get(0).findValue("route").findValue("lngFrom").toString());
+            double lngTo = Double.parseDouble(actualObj.get("data").get(0).findValue("route").findValue("lngTo").toString());
+            double diffhours = Math.abs(lngTo-lngFrom)*4/60;
+            double productivity = JetLagMap.calculateProductivity(diffhours);
+
+            return new ContributorResult(price, productivity, 1);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return ContributorResult.Companion.getDefault();
+        return ContributorResult.Companion.createDefault();
 
     }
 }
--- src/main/java/com/treecio/meetpoint/model/contributors/JetLagContributor.java
+++ src/main/java/com/treecio/meetpoint/model/contributors/JetLagContributor.java
@@ -1,7 +1,22 @@
 package com.treecio.meetpoint.model.contributors;
 
+import com.treecio.meetpoint.kiwi.DestinationImpossible;
+import com.treecio.meetpoint.model.Contributor;
+import com.treecio.meetpoint.model.ContributorResult;
+import com.treecio.meetpoint.model.MeetingPossibility;
+import com.treecio.meetpoint.model.db.User;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by Pali on 24.06.2017.
  */
-public class JetLagContributor {
+public class JetLagContributor implements Contributor{
+
+
+    @NotNull
+    @Override
+    public ContributorResult process(@NotNull MeetingPossibility cr, @NotNull User user) throws DestinationImpossible {
+
+        return null;
+    }
 }
