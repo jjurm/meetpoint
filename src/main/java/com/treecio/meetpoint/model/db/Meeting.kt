d2cb56bd96fc73a607c234efd7083c64ba696ead
Juraj Mičko <micko.juraj64@gmail.com>
2017-06-24 15:04:06 +0200

    Change Meeting model

--- src/main/java/com/treecio/meetpoint/model/db/Meeting.kt
+++ src/main/java/com/treecio/meetpoint/model/db/Meeting.kt
@@ -5,19 +5,31 @@ import java.sql.PreparedStatement
 import java.util.*
 
 class Meeting(id: Int,
-                   val name: String,
-                   val startDate: java.util.Date,
-                   val endDate: java.util.Date): DatabaseObject(id) {
+              val name: String,
+              val startDate: java.util.Date,
+              val endDate: java.util.Date,
+              val budget: Int,
+              val prioBudget: Int,
+              val prioHappiness: Int,
+              val prioProductivity: Int,
+              val offices: String
+) : DatabaseObject(id) {
 
     override fun getTable(): String = "meetings"
-    override fun getColumns(): Array<String> = arrayOf("name", "startDate", "endDate")
+    override fun getColumns(): Array<String> = arrayOf("name", "startDate", "endDate", "budget", "prio_budget", "prio_happiness", "prio_productivity", "offices")
     override fun bindValues(stmt: PreparedStatement) {
         stmt.setString(1, name)
         stmt.setLong(2, startDate.time)
         stmt.setLong(3, endDate.time)
+        stmt.setInt(4, budget)
+        stmt.setInt(5, prioBudget)
+        stmt.setInt(6, prioHappiness)
+        stmt.setInt(7, prioProductivity)
+        stmt.setString(8, offices)
     }
 
-    constructor(name: String, startDate: Date, endDate: Date) : this(-1, name, startDate, endDate)
+    constructor(name: String, startDate: Date, endDate: Date, budget: Int, prioBudget: Int, prioHappiness: Int, prioProductivity: Int, offices: String) :
+            this(-1, name, startDate, endDate, budget, prioBudget, prioHappiness, prioProductivity, offices)
 
     companion object {
         fun querySel(selection: String): Meeting? {
@@ -26,11 +38,17 @@ class Meeting(id: Int,
                 return Meeting(rs.getInt("id"),
                         rs.getString("name"),
                         Date(rs.getLong("startDate")),
-                        Date(rs.getLong("endDate"))
+                        Date(rs.getLong("endDate")),
+                        rs.getInt("budget"),
+                        rs.getInt("prio_budget"),
+                        rs.getInt("prio_happiness"),
+                        rs.getInt("prio_productivity"),
+                        rs.getString("offices")
                 )
             }
             return null
         }
+
         fun query(id: Int): Meeting? {
             return querySel("id = " + id)
         }
